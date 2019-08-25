43bc61510f2e54a6355b1d5e67b2b3f6f48af485
Moogle2346 <Tnkd1422>
2019-08-25 23:19:23 +0900

    CSV読み込み(改行未対応)

--- Test_CsvReader/Form1.cs
+++ Test_CsvReader/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,66 @@ namespace Test_CsvReader
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> data = new List<string>();
+            string path = @"C:\Users\PCuser\Desktop\test.csv";
+            string tmp;
+
+            try
+            {
+                //ファイルが存在するか確認
+                if (File.Exists(path) == true)
+                {
+                    using (StreamReader sr = new StreamReader(path, Encoding.GetEncoding("Shift-JIS")))
+                    {
+                        //EOFまで読み取り
+                        while (sr.Peek() > -1)
+                        {
+                            // Anser = """12","3,4","5,6"""
+                            tmp = sr.ReadLine();
+                            int DoubleQuote = 0;
+                            int StartPosition = 0;
+                            bool QuoteExist = false;
+
+                            for (int i = 0; i < tmp.Length; i++)
+                            {
+                                if (tmp[i] == '"')
+                                {
+                                    DoubleQuote++;
+                                    if(QuoteExist == false)
+                                    {
+                                        StartPosition = i + 1;
+                                        QuoteExist = true;
+                                    }
+                                }
+
+                                if ((tmp[i] == ',' && DoubleQuote % 2 == 0) || (i == tmp.Length-1))
+                                {
+                                    if (QuoteExist == true)
+                                    {
+                                        string tmp2 = tmp.Substring(StartPosition, i - StartPosition - 1);
+                                        data.Add(tmp2.Replace("\"\"", "\""));
+                                    }
+                                    else
+                                    {
+                                        data.Add(tmp.Substring(StartPosition, i - StartPosition));
+                                    }
+                                    StartPosition = i;
+                                    QuoteExist = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            ;
         }
     }
 }
